0995a99c33486e332829904ba253b19e08623923
Michael Ernst <mernst@cs.washington.edu>
2018-07-13 06:58:29 -0700

    lint-diff.py: --strip-diff and --strip-lint command-line options

--- lint-diff.py
+++ lint-diff.py
@@ -2,8 +2,10 @@
 
 # Filter the ouput of lint, to only show output for changed lines.
 
-# Usage:  lint-diff.py diff.txt [lint-output.txt]
+# Usage:  lint-diff.py [options] diff.txt [lint-output.txt]
 #         If lint-output is omitted, use standard input.
+# Options: --strip-diff=N means to ignore N leading "/" in diff.txt.
+#          --strip-lint=N means to ignore N leading "/" in lint-output.txt.
 # Output: all lines in lint-output that are on a changed line
 #         Output status is 1 if it produced any output, 0 if not, 2 if error.
 
@@ -15,15 +17,50 @@
 
 from __future__ import print_function
 
+import os
 import re
 import sys
+
+strip_diff = 0
+strip_lint = 0
+
 
 def eprint(*args, **kwargs):
     """Print to stderr."""
     print(*args, file=sys.stderr, **kwargs)
 
+def strip_dirs(filename, num_dirs):
+    """Strip off `num_dirs` leading "/" characters."""
+    if num_dirs == 0:
+        return filename
+    else:
+        return os.path.join(*(filename.split(os.path.sep)[num_dirs:]))
+
+### Main routine
+
+# True if the diff filenames start with "a/" and "b/".
+relative_diff = False
+# True if a warning has been issued about relative directories.
+relative_diff_warned = False
+
+# TODO: Use argparse instead?  I don't see how to indicate that
+# the lint-output.txt argument is optional.
+while len(sys.argv) > 1 and sys.argv[1].startswith("--strip-"):
+    m = re.match('^--strip-diff=([0-9]+)$', sys.argv[1])
+    if m:
+        strip_diff = int(m.group(1))
+        del sys.argv[1]
+        continue
+    m = re.match('^--strip-lint=([0-9]+)$', sys.argv[1])
+    if m:
+        strip_lint = int(m.group(1))
+        del sys.argv[1]
+        continue
+    eprint("Bad argument:", sys.argv[1])
+    sys.exit(2)
+
 if len(sys.argv) != 2 and len(sys.argv) != 3:
-    eprint(sys.argv[0], " needs 1 or 2 arguments, got ", len(sys.argv)-1)
+    eprint(sys.argv[0], "needs 1 or 2 arguments, got", len(sys.argv)-1)
     sys.exit(2)
 
 # A dictionary from file names to a set of ints
@@ -32,8 +69,9 @@ changed = {}
 # 1 if this produced any output, 0 if not
 status = 0
 
-with open(sys.argv[1]) as diff:
-    ppp_re = re.compile('\+\+\+ (b/)?(\S*).*')
+diff_filename = sys.argv[1]
+with open(diff_filename) as diff:
+    ppp_re = re.compile('\+\+\+ (\S*).*')
     atat_re = re.compile('@@ -([0-9]+)(,[0-9]+)? \+([0-9]+)(,[0-9]+)? @@.*')
     content_re = re.compile('[ +-].*')
 
@@ -44,7 +82,9 @@ with open(sys.argv[1]) as diff:
             continue
         m = ppp_re.match(diff_line)
         if m:
-            filename = m.group(2)
+            if m.group(1).startswith("b/"): # heuristic
+                relative_diff = True
+            filename = strip_dirs(m.group(1), strip_diff)
             if filename not in changed:
                 changed[filename] = set()
             continue
@@ -60,9 +100,14 @@ with open(sys.argv[1]) as diff:
         if diff_line.startswith("-"):
             continue
 
+if relative_diff and strip_diff == 0:
+    eprint("warning:", sys.argv[1], "may use relative paths but --strip-diff=0")    
+
 if len(sys.argv) == 3:
-    lint = open(sys.argv[2])
+    lint_filename = sys.argv[2]
+    lint = open(lint_filename)
 else:
+    lint_filename = "stdin"
     lint = sys.stdin
 
 filename_lineno_re = re.compile('([^:]*):([0-9]+):.*')
@@ -70,7 +115,11 @@ filename_lineno_re = re.compile('([^:]*):([0-9]+):.*')
 for lint_line in lint:
     m = filename_lineno_re.match(lint_line)
     if m:
-        filename = m.group(1)
+        filename = strip_dirs(m.group(1), strip_lint)
+        if filename.startswith("/") and relative_diff and strip_lint == 0:
+            if not relative_diff_warned:
+                eprint("warning:", sys.argv[1], "uses relative paths but", lint_filename, "uses absolute paths")
+                relative_diff_warned = True
         lineno = int(m.group(2))
         if (filename in changed and lineno in changed[filename]):
             print(lint_line, end='')
