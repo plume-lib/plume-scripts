9548f5bd72c7bbfe31f05b615b6ed4e30da65648
Michael Ernst <mernst@cs.washington.edu>
2020-02-20 05:23:58 -0800

    Rewrite ci-last-success.py, but abandon it because api.github.com is not reliable

--- ci-last-success.py
+++ ci-last-success.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
+"""Usage:  ci-last-success ORG REPO [CANDIDATE]
 
-# Usage:  ci-last-success ORG REPO
-#
-# Outputs the SHA commit id corresponding to the most recent successful CI job.
-# Currently works only for Azure Pipelines, and only for the master branch.
-#
-# Requires the Python requests module to be installed.
+Outputs the SHA commit id corresponding to the most recent successful CI job
+that is CANDIDATE (a SHA hash) or earlier.
+Currently works only for Azure Pipelines.
+
+Requires the Python requests module to be installed.
+"""
 
 # This does no GitHub authentication, so it is limited to 60 requests per
 # hour.  It fails (and prints nothing to standard out, only to standard
@@ -13,34 +14,69 @@
 
 import json
 import pprint
-import requests
+import subprocess
 import sys
 
-if len(sys.argv) != 3:
-    print('Wrong number of arguments {}, expected 2'.format(len(sys.argv)-1))
+import requests
+
+DEBUG = False
+# DEBUG=True
+
+if len(sys.argv) != 3 and len(sys.argv) != 4:
+    print('Wrong number of arguments {}, expected 2 or 3'.format(len(sys.argv) - 1))
     sys.exit(2)
 
-org=sys.argv[1]
-repo=sys.argv[2]
+org = sys.argv[1]
+repo = sys.argv[2]
+if len(sys.argv) == 4:
+    commit_arg = sys.argv[3]
+else:
+    gitRevParseResult = subprocess.run(["git", "rev-parse", "HEAD"], capture_output=True)
+    if gitRevParseResult.returncode == 0:
+        commit_arg = gitRevParseResult.stdout.rstrip().decode("utf-8")
+    else:
+        raise Exception(gitRevParseResult)
 
-url_commits = 'https://api.github.com/repos/{}/{}/commits'.format(org, repo)
-resp_commits = requests.get(url_commits)
-if resp_commits.status_code != 200:
-    # This means something went wrong, possibly rate-limiting.
-    raise Exception('GET {} {} {}'.format(url_commits, resp_commits.status_code, resp_commits.headers))
-for commit in resp_commits.json():
-    sha=commit['sha']
+if DEBUG:
+    print("commit_arg: {}".format(commit_arg))
+
+
+### PROBLEM: api.github.com is returning   "state": "pending"   for commits with completed CI jobs.
+### Maybe I need to screen-scrape a different github.com page.  :-(
+def successful(sha):
+    "Return true if SHA's CI job succeeded."
     # message=commit['commit']['message']
     url_status = 'https://api.github.com/repos/{}/{}/commits/{}/status'.format(org, repo, sha)
+    if DEBUG:
+        print(url_status)
     resp_status = requests.get(url_status)
     if resp_status.status_code != 200:
         # This means something went wrong, possibly rate-limiting.
-        raise Exception('GET {} {} {}'.format(url_status, resp_status.status_code, resp_status.headers))
-    state=resp_status.json()['state']
-    if (state == "success"):
-        print('{}'.format(sha))
-        sys.exit(0)
+        raise Exception('GET {} {} {}'.format(url_status, resp_status.status_code,
+                                              resp_status.headers))
+    state = resp_status.json()['state']
+    return state == "success"
 
-# Default to the penultimate commit.
-print('{}'.format(resp_commits.json()[1]['sha']))
-sys.exit(0)
+
+def parent(sha):
+    "Return the SHA of the first parent of the given SHA.  Return None if this is the root."
+    getParentResult = subprocess.run(["git", "rev-parse", sha + "^"], capture_output=True)
+    if getParentResult.returncode != 0:
+        return None
+    return getParentResult.stdout.rstrip().decode("utf-8")
+
+
+commit = commit_arg
+while True:
+    if DEBUG:
+        print("Testing {}".format(commit))
+    if successful(commit):
+        print('{}'.format(commit))
+        sys.exit(0)
+    the_parent = parent(commit)
+    if the_parent is None:
+        print('{}'.format(parent(commit_arg)))
+        sys.exit(0)
+    commit = the_parent
+
+sys.exit(1)
