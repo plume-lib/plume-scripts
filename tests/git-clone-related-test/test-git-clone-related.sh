dcda8da7bfa3963532fd80e62899821922915625
Michael Ernst <mernst@cs.washington.edu>
2020-08-29 20:26:23 -0700

    Use absolute, not relative, directory

--- tests/git-clone-related-test/test-git-clone-related.sh
+++ tests/git-clone-related-test/test-git-clone-related.sh
@@ -22,14 +22,15 @@ set -o errexit -o nounset
 # set -v : Display shell input lines as they are read.
 
 USER=${USER:-git-clone-related}
-
+PLUME_SCRIPTS=$(cd ../../; pwd -P)
 startdir=mytmpdir=$(mktemp -d 2>/dev/null || mktemp -d -t 'startdir')
 goaldir=mytmpdir=$(mktemp -d 2>/dev/null || mktemp -d -t 'goaldir')
 rm -rf "$startdir" "$goaldir"
+
 git clone --branch "$START_BRANCH" "$START_REPO" "$startdir" -q --single-branch --depth 1
 # $ARGS should not be quoted
 # shellcheck disable=SC2086
-(cd "$startdir" && ../../git-clone-related $ARGS "$goaldir")
+(cd "$startdir" && "${PLUME_SCRIPTS}"/git-clone-related $ARGS "$goaldir")
 clonedrepo=$(git -C "$goaldir" config --get remote.origin.url)
 clonedbranch=$(git -C "$goaldir" branch --show-current)
 
